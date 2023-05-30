bce2ed7c30233cbbb99ece94f303fd5e5a0c0ebb
Ramazan ÇETİN <36192021+ramazanlcetin@users.noreply.github.com>
2023-05-30 12:38:03 +0300

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -5,3 +5,18 @@ AS ENUM ('RESEARCH','COMPILATION','FACT_PRESENTATION','OTHER','BOOK_PRESENTATION
 
 --Add Property On Custom Type (Özel Tipe Veri Veri Ekleme)
 ALTER TYPE enum_publication_type ADD VALUE  IF NOT EXISTS  'NOTE';
+
+
+-- Other DB connect table of view (Başka bir DB üzerindeki tabloyu view olarak bağlama)
+CREATE OR REPLACE VIEW public.vw_user_list
+AS SELECT t1.id,
+    t1.first_name,
+    t1.last_name,
+    t1.email,
+    t1.old_id,
+    t1.enabled,
+    (t1.first_name::text || ' '::text) || t1.last_name::text AS fullname
+   FROM dblink('host=mydomain.address dbname=DB_NAME  
+               user=USER_NAME password=PASSWORD'::text, 
+               'select u.id,u.first_name,u.last_name,u.email,u.enabled from tbl_user_list as u'::text) 
+               t1(id character varying, first_name character varying, last_name character varying, email character varying, enabled boolean);
